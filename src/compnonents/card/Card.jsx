cb81a828e8866c4d60baaaa47ee45e6a532e7115
rohan <rohanthakur89768@gmail.com>
2024-06-28 13:21:12 +0530

    aded logo

--- src/compnonents/card/Card.jsx
+++ src/compnonents/card/Card.jsx
@@ -1,3 +1,11 @@
+import pick1 from '../../assets/nikk1.jpg'
+import pick2 from '../../assets/nikk2.jpg'
+import pick3 from '../../assets/nikk3.jpg'
+import pick4 from '../../assets/nikk4.jpg'
+
+
+
+
 const Cardd = () => {
             return (
               <div className="flex flex-wrap  justify-center">
@@ -5,12 +13,12 @@ const Cardd = () => {
                     <div className="rounded overflow-hidden shadow-lg mx-auto">
                       <img
                         className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/4cp4_7bdfd337-1f8c-4354-ad85-6f00bc57ebf6.jpg?v=1661501210&width=360"
+                        src={pick1}
                         alt="Areca Leaf Bowl"
                       />
                       <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Eco-Friendly Disposable Compostable 4 Compartment Dinner Plates, Pack of 25, Off White (4CP, R)</div>
-                        <p className="text-gray-700 text-base">₹450</p>
+                        <div className="font-bold text-xl mb-2">8 inch plates 25 in one pack </div>
+                        <p className="text-gray-700 text-base">₹200</p>
                       </div>
                     </div>
                   </div>
@@ -20,12 +28,12 @@ const Cardd = () => {
                     <div className="rounded overflow-hidden shadow-lg mx-auto">
                       <img
                         className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/biodegrable9inchesplate.jpg?v=1661500753&width=360"
+                        src={pick2}
                         alt="Areca Leaf Bowl"
                       />
                       <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Eco-Friendly Disposable Square Dinner Plates, Pack of 25, Off White (9 inch, R)</div>
-                        <p className="text-gray-700 text-base">₹240</p>
+                        <div className="font-bold text-xl mb-2"> 12 inch plates 25 in one pack  </div>
+                        <p className="text-gray-700 text-base">₹125</p>
                       </div>
                     </div>
                   </div>
@@ -35,12 +43,12 @@ const Cardd = () => {
                     <div className="rounded overflow-hidden shadow-lg mx-auto">
                       <img
                         className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/3cpplates.jpg?v=1661501041&width=360"
+                        src={pick3}
                         alt="Areca Leaf Bowl"
                       />
                       <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Casa De Amor Eco-Friendly Disposable 3 Compartment Dinner Plates Off White (Pack of 25)</div>
-                        <p className="text-gray-700 text-base">₹300</p>
+                        <div className="font-bold text-xl mb-2">4 inch Dona (Pack of 50)</div>
+                        <p className="text-gray-700 text-base">₹50</p>
                       </div>
                     </div>
                   </div>
@@ -49,76 +57,27 @@ const Cardd = () => {
                     <div className="rounded overflow-hidden shadow-lg mx-auto">
                       <img
                         className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/glass150ml.jpg?v=1660235311&width=360"
+                        src={pick4}
                         alt="Areca Leaf Bowl"
                       />
                       <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Ripple Paper Disposable Tea / Coffee Cups, 150 ml - Brown (Pack of 50)
+                        <div className="font-bold text-xl mb-2">10 inch Plates (Pack of 25)
                         </div>
-                        <p className="text-gray-700 text-base">₹200</p>
+                        <p className="text-gray-700 text-base">₹150</p>
                       </div>
                     </div>
                   </div>
 
-                  <div  className="w-1/2 sm:w-1/2 p-2">
-                    <div className="rounded overflow-hidden shadow-lg mx-auto">
-                      <img
-                        className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/platecombo.jpg?v=1660392256&width=360"
-                        alt="Areca Leaf Bowl"
-                      />
-                      <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Sugarcane Bagasse Disposable Plates Set, Square Plates 9 Inch (25), Square Plates 6 Inch (25), Bowls (25), Spoon (50), Brown Party Disposal Biodegradable</div>
-                        <p className="text-gray-700 text-base">₹500</p>
-                      </div>
-                    </div>
-                  </div>
+                  
 
 
-                  <div  className="w-1/2 sm:w-1/2 p-2">
-                    <div className="rounded overflow-hidden shadow-lg mx-auto">
-                      <img
-                        className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/disposablebowl3.jpg?v=1661501069&width=360"
-                        alt="Areca Leaf Bowl"
-                      />
-                      <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Eco-Friendly All Natural Disposable Compostable 120 ml Bowl (Pack of 25)</div>
-                        <p className="text-gray-700 text-base">₹100</p>
-                      </div>
-                    </div>
-                  </div>
+                  
 
 
-                  <div  className="w-1/2 sm:w-1/2 p-2">
-                    <div className="rounded overflow-hidden shadow-lg mx-auto">
-                      <img
-                        className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/spoon_c3cae83c-4905-4e29-a10b-4d98da7b9644.jpg?v=1660386248&width=360"
-                        alt="Areca Leaf Bowl"
-                      />
-                      <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Disposable Wooden Spoons, 140 mm | Spoon for Home Use | Eco-Friendly | Disposable Spoon (Pack of 100)</div>
-                        <p className="text-gray-700 text-base">₹200</p>
-                      </div>
-                    </div>
-                  </div>
+                 
 
 
-
-                  <div  className="w-1/2 sm:w-1/2 p-2">
-                    <div className="rounded overflow-hidden shadow-lg mx-auto">
-                      <img
-                        className="  object-cover"
-                        src="https://casadeamor.in/cdn/shop/products/plate6inches-2_a18215b0-7f96-4084-b8fd-dcf5db991f0c.jpg?v=1661500832&width=360"
-                        alt="Areca Leaf Bowl"
-                      />
-                      <div className="px-2 py-4">
-                        <div className="font-bold text-xl mb-2">Bagasse Eco Friendly, Disposable 6 inch Plates Square Meal Dinner Plate (Pack of 25, Microwave Safe)</div>
-                        <p className="text-gray-700 text-base">₹250</p>
-                      </div>
-                    </div>
-                  </div>
+                  
 
 
 
